bd67e32552f8bc3d77d09f1e27adaa1dd11e7b08
yanagidahana <hana878787zzz@gmail.com>
2021-01-05 20:55:39 +0900

    main.js

--- 020_スクロール×文字アニメーション/main.js
+++ 020_スクロール×文字アニメーション/main.js
@@ -1,23 +1,21 @@
-const child = document.querySelector('.child');
-const cb = function(entrees, observe) {
-  entrees.forEach(entry => {
-    if(entry.isIntersecting) {
-      console.log('inview');
-      // observe.unobserve(entry.target);
-      entry.target.classList.add('inview');
+document.addEventListener('DOMContentLoaded', function () {
 
-    } else {
-      console.log('out view');
-      entry.target.classList.remove('inview');
-
-    }
-  });
-}
-  // alert('intersecting');
-const options  = {
-  root: null,
-  rootMargin: "-200px 0px 0px 0px",
-  threshold: 1
-};
-const io = new IntersectionObserver(cb,options);
-io.observe(child);
+  const els = document.querySelectorAll('.animate-title');
+  const cb = function (entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const ta = new TextAnimation(entry.target);
+              ta.animate();
+              observer.unobserve(entry.target);
+          } else {
+          }
+      });
+  };
+  const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0
+  };
+  const io = new IntersectionObserver(cb, options);
+  els.forEach(el => io.observe(el));
+});
