3ef83e9bbc9041510d3d171048046b76e9c4752d
yanagidahana <hana878787zzz@gmail.com>
2021-01-06 22:44:24 +0900

    map2

--- EC6/map.js
+++ EC6/map.js
@@ -2,3 +2,14 @@ var doubledNumbers = [];
 doubledNumbers.push(number[i]*2);
 return number * 2;
 doubledNumbers;
+
+var cars = [
+  {type: '軽自動車', price: '安い'},
+  {type:'高級車', price: '高い'}
+];
+
+var price = cars.map(function(car) {
+  return car.price;
+});
+
+prices;
